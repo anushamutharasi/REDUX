388d4f0900809f503d08fefa13ea3d73de40d8fa
anushamutharasi <anushamutharasi@gmail.com>
2025-01-29 22:05:13 +0530

    redux-coding

---
+++ redux_beginner/src/Redux/Store.js
@@ -0,0 +1,37 @@
+//Using only redux
+import { createStore } from "redux";
+
+//intilization of state
+
+const initialState = {
+  firstName: "Preveen",
+  lastName: "",
+};
+
+//creating a reducer
+
+function LoginReducer(state = initialState, action) {
+  console.log("step3");
+  switch (action.type) {
+    case "FIRSTNAME":
+      return { ...state, firstName: (state.firstName = action.payload) };
+    case "LASTNAME":
+      return { ...state, lastName: (state.lastName = action.payload) };
+
+    default:
+      return state;
+  }
+}
+
+//passing reducer to creating store function
+console.log(createStore);
+ const store = createStore(LoginReducer);
+console.log(store);
+
+console.log("step1")
+console.log(store.getState());
+console.log("step2");
+
+store.dispatch({type:"FIRSTNAME",payload:"Anusha "})
+store.dispatch({ type: "LASTNAME", payload: "Muthrasi" });
+console.log(store.getState())
